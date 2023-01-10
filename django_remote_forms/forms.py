5e818fd183a4db7e693f905abd4bf35468d71f8a
Sirres <bconnectlu@gmail.com>
2023-01-10 13:53:36 +0100

    Insert order of dict items is guaranteed in python 3.7+

--- django_remote_forms/forms.py
+++ django_remote_forms/forms.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 from django_remote_forms import fields, logger
 from django_remote_forms.utils import resolve_promise
 
@@ -107,13 +105,13 @@ class RemoteForm(object):
             }
         }
         """
-        form_dict = OrderedDict()
+        form_dict = {}
         form_dict['title'] = self.form.__class__.__name__
         form_dict['non_field_errors'] = self.form.non_field_errors()
         form_dict['label_suffix'] = self.form.label_suffix
         form_dict['is_bound'] = self.form.is_bound
         form_dict['prefix'] = self.form.prefix
-        form_dict['fields'] = OrderedDict()
+        form_dict['fields'] = {}
         form_dict['errors'] = self.form.errors
         form_dict['fieldsets'] = getattr(self.form, 'fieldsets', [])
 
